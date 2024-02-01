79f019fb442ef582cb3f3065edfaecff056131ae
Shifat <a.shifat@iitg.ac.in>
2024-02-01 22:56:33 +0530

    added database connectivity and env fiel

---
+++ backend/database/pool.js
@@ -0,0 +1,31 @@
+const app = require("express")();
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    max: 20,
+    connectionTimeoutMillis: 3000,
+    idleTimeoutMillis: 30000,
+});
+
+
+app.get("/users", async (req, res) => {
+    const fromDate = new Date();
+
+    //return all rows
+    const results = await pool.query("select username, email from users")
+    console.table(results.rows)
+    console.log(new Date())
+    const toDate = new Date();
+    const elapsed = toDate.getTime() - fromDate.getTime();
+
+    //send it to the wire
+    res.send({ "rows": results.rows, "elapsed": elapsed, "method": "pool" })
+})
+
+app.listen(2015, () => console.log("Listening on port 2015"))
