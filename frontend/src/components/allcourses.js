8660d9e453608aed6ea7c5771649983ba8d80083
AyushSavar <ayush.savar1@gmail.com>
2024-02-10 23:55:29 +0530

    final changes

--- frontend/src/components/allcourses.js
+++ frontend/src/components/allcourses.js
@@ -1,19 +1,54 @@
 import React from "react";
 import Card from "./card";
 import { useNavigate } from 'react-router';
+import { useEffect ,useState} from "react";
+import axios from "axios";
+import Loading from './Loading';
+import { Navigate } from "react-router";
 
 export default function AllCourses() {
-
+  
   const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  var courses = null;
+  var prev = null;
+  var next = null;
+
+  useEffect(() => {
+  
+    const fetchData = async () => {
+      try {
+        
+        const response = await axios.get('http://localhost:2015/courses/',{ params: { page: 1, limit: 10 }});
+        //console.log(response.data.courses);
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+  
+  courses = data.courses;
+  prev = data.previous;
+  next = data.next;
+
+  if (loading) {
+    return <p><Loading/></p>;
+  }
+
 
     return <>
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2,1fr)', gap: '25px' }}>
-    
-      <div  onClick={() => navigate(':courseId')} style={{ height: '210px',backgroundColor:'#aac0fb', borderRadius:'20px',cursor: 'pointer' }}><Card/></div>
-      <div  onClick={() => navigate(':courseId')} style={{ height: '210px',backgroundColor:'#aac0fb', borderRadius:'20px',cursor: 'pointer' }}><Card/></div>
-      <div  onClick={() => navigate(':courseId')} style={{ height: '210px',backgroundColor:'#aac0fb', borderRadius:'20px',cursor: 'pointer' }}><Card/></div>
-      <div  onClick={() => navigate(':courseId')} style={{ height: '210px',backgroundColor:'#aac0fb', borderRadius:'20px',cursor: 'pointer' }}><Card/></div>
-      <div  onClick={() => navigate(':courseId')} style={{ height: '210px',backgroundColor:'#aac0fb', borderRadius:'20px',cursor: 'pointer' }}><Card/></div>
+      {courses.map((course, index) => (
+        <div  onClick={() => navigate(':courseId')} style={{ height: '210px',backgroundColor:'#aac0fb', borderRadius:'20px',cursor: 'pointer' }}>
+        <Card key={index} data={course} />
+        </div>
+      ))}
+      <Card/>
     </div>
      
     </>;
--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -3,26 +3,49 @@ import { BarChart } from '@mui/x-charts/BarChart';
 
 
 
-export default function Card() {
+export default function Card(props) {
+
+//console.log(props.data.title);
+if (props.data)
+{
+ const course = {
+    "title": props.data.title,
+    "overall": props.data.overallRate,
+    "ones": props.data.ratings.ones,
+    "twos": props.data.ratings.twos,
+    "threes": props.data.ratings.threes,
+    "fours": props.data.ratings.fours,
+    "fives": props.data.ratings.fives,
+    "number": props.data.numOfRates,
+  }
 
     
+    let onecount=parseInt(course.ones);
+    let twocount=parseInt(course.twos);
+    let threecount=parseInt(course.threes);
+    let fourcount=parseInt(course.fours);
+    let fivecount=parseInt(course.fives);
+    
+
     return <>
+
        <div style={{height:'90%',width:'100%',display:'flex', flexDirection: "column"}}>
          <div style={{flex:1,display:'flex',flexDirection: "column",marginLeft:'10px'}}>
-            <div  style={{flex:1,fontSize:'40px'}}>ME 101</div>
-            <div  style={{flex:1,fontSize:'15px',marginTop:'-8px'}}>Engineering Mechanics</div>
+            <div  style={{flex:1,fontSize:'40px'}}>{course.title}</div>
+            <div  style={{flex:1,fontSize:'40px'}}>{course.description}</div>
+            
          </div>
          <div style={{flex:2 ,display:'flex'}}>
             <div style={{flex:1 ,marginLeft:'10px',display:'flex',flexDirection: "column"}}>
                 <div  style={{flex:4}}></div>
-                <div  style={{flex:1,fontSize:'12px',marginLeft:'5px'}}>400 ratings</div>
+                <div  style={{flex:1,fontSize:'12px',marginLeft:'5px'}}>{course.number} ratings</div>
             </div>
             <div style={{flex:3 ,display:'flex'}}>
             <div style={{flex:0.5}}></div>
                 <div style={{flex:6}}>
                 <BarChart
                     xAxis={[{ scaleType: 'band', data: ['1', '2', '3','4','5'] }]}
-                    series={[{ data: [40, 3, 5,3,4] }]}
+                    series={[{ data: [onecount, twocount, threecount,fourcount,fivecount] }]}
                     margin={{ left: 50, right: 5, top: 40, bottom: 28 }}
                     width={150}
                     height={120}
@@ -32,7 +55,7 @@ export default function Card() {
                 <div style={{flex:1}}>
                     <div style={{height:'70px'}}></div>
                     <div style={{height:'50px', display: 'flex',flexDirection: "column", alignItems: 'center', justifyContent: 'center'}}>
-                        <h1>4.5</h1>
+                        <h1>{course.overall}</h1>
                         <h1 style={{marginTop:'-5px',marginBottom:'15px',paddingRight:'12px'}}>*****</h1>
                     </div>
                 </div>
@@ -41,5 +64,6 @@ export default function Card() {
          </div>
        </div>
     </>;
+}
   }
   
